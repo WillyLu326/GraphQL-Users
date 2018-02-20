a1f3973e47c2a94feaca4f5b3f3c1bb23a03a4f6
WillyLu326 <luzh0326@gmail.com>
2018-02-19 23:16:24 -0500

    create graphQL server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const graphQLHTTP = require('express-graphql');
+const shcema = require('./schemas/schema');
 
 const app = express();
+
+app.use('/graphql', graphQLHTTP({
+  schema,
+  graphiql: true
+}));
 
 app.listen(8080, () => console.log('Start Listening Port 8080'));
